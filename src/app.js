3c34408d2327f7116528f1a007e3fca97fe5c8a2
Lucía vega <luciavf98@gmail.com>
2023-12-16 17:28:26 +0100

    creado editar news

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import "dotenv/config.js";
 import express from "express";
 import connectionDB from "../db";
 import bcrypt from "bcrypt";
-import bodyParser from "body-parser";
-import jwt from "jsonwebtoken";
 
 const app = express();
 
@@ -11,6 +9,61 @@ const PORT = Number(process.env.MYSQL_PORT);
 
 app.listen(PORT || 3000, () => {
   console.log(`Escuchando http://localhost:${PORT}`);
+});
+
+const jsonParser = express.json();
+
+app.post("/register", jsonParser, async (req, res) => {
+  const { nickName, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  await db.execute(
+    `INSERT INTO users(nickName, email, password) 
+        VALUES (?, ?, ?)`,
+    [nickName, email, hashedPassword]
+  );
+
+  res.status(200).send("Fué Registrado Exitosamente !");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const [result] = await db.execute(
+    `SELECT * FROM users WHERE email = ? LIMIT = 1`,
+    [email]
+  );
+  const maybeUser = result[0];
+  if (!maybeUser) {
+    res.status(400).json({
+      error: "Credenciales NO validas",
+    });
+    return;
+  }
+
+  const doesPasswordMatch = await bcrypt.compare(password, maybeUser.password);
+  if (!doesPasswordMatch) {
+    res.status(400).json({
+      error: "Credenciales NO validas",
+    });
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      id: maybeUser.id,
+      nickName: maybeUser.nickName,
+      email: maybeUser.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "5d",
+    }
+  );
+  res.status(200).json({
+    token,
+  });
 });
 
 app.use(bodyParser.json());
@@ -27,18 +80,28 @@ app.post("/news", verifyToken, (req, res) => {
   });
 });
 
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
+app.put("/news/:idNews", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const newsId = req.params.idNews;
+      const updatedNewsData = req.body;
 
-  if (typeof bearerHeader !== "undefined") {
-    const bearerToken = bearerHeader.split(" ")[1];
-    req.token = bearerToken;
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-}
+      res.json({
+        message: "Noticia editada con éxito",
+      });
+    }
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+app.delete("/delete/news/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      `DELETE * FROM news WHERE id = ?`,
+        res.json({ message: "Noticia eliminada con éxito" });
+    }
+  });
 });
