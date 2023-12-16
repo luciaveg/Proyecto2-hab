d76ed2cf609394e86d730728ea3f68796af70b59
Lucía vega <luciavf98@gmail.com>
2023-12-16 17:27:11 +0100

    creado editar news

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import "dotenv/config.js";
 import express from "express";
 import connectionDB from "../db";
 import bcrypt from "bcrypt";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
@@ -9,4 +11,34 @@ const PORT = Number(process.env.MYSQL_PORT);
 
 app.listen(PORT || 3000, () => {
   console.log(`Escuchando http://localhost:${PORT}`);
+});
+
+app.use(bodyParser.json());
+
+app.post("/news", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      const newNews = req.body;
+
+      res.json({ message: "Noticia creada con éxito", data: newNews });
+    }
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+
+  if (typeof bearerHeader !== "undefined") {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
