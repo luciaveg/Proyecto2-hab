bb564d5ae5c492931c8169c15f36685fa68f357b
Rafael <rafaelmaitin3@gmail.com>
2024-01-15 21:21:37 +0100

    Actualizacion

--- src/db/connection-db.js
+++ src/db/connection-db.js
@@ -1,26 +1,3 @@
-import mysql from "mysql2/promise";
-import "dotenv/config.js";
-
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
-
-const connectDB = async () => {
-  try {
-    const connection = await mysql.createConnection({
-      host: MYSQL_HOST,
-      user: MYSQL_USER,
-      password: MYSQL_PASS,
-      database: MYSQL_DB,
-    });
-
-    console.log("Conexión a la base de datos exitosa.");
-
-    return connection;
-  } catch (error) {
-    console.error("Error al conectar a la base de datos:", error);
-    throw error;
-  }
-};
-
-import createConnectionPool from "./db.js";
+import createConnectionPool from "./create-pool.js";
 const db = createConnectionPool(process.env.MYSQL_DB);
 export default db;
